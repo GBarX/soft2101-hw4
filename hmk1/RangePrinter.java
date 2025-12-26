0d7b21f3732dea691c369483285e09c96f43a386
Berk Göksu <goksuberk34@gmail.com>
2025-12-26 19:30:16 +0300

    Satifying requirement one

---
+++ hmk1/RangePrinter.java
@@ -0,0 +1,23 @@
+package hmk1;
+
+import java.util.Scanner;
+
+public class RangePrinter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first num: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter second num: ");
+        int num2 = scanner.nextInt();
+
+        printRange(num1, num2);
+    }
+
+    public static void printRange(int start, int end) {
+        for (int i = start + 1; i < end; i++) {
+            System.out.println(i);
+        }
+    }
+}
